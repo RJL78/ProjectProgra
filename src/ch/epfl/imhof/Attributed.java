07b11963f8845f42ac711d6ec8bc366c6f8e7f93
Raphael <raphael.laporte@live.com>
2015-03-05 17:48:46 +0100

    Attributed<T> class added

---
+++ src/ch/epfl/imhof/Attributed.java
@@ -0,0 +1,37 @@
+package ch.epfl.imhof;
+
+public final class Attributed<T> {
+
+    private T inputValue;
+    private Attributes attributes; 
+    
+    public Attributed (T value, Attributes attributes){
+        inputValue = value; 
+        this.attributes = attributes;
+    }
+    
+    public T value (){
+        return inputValue;
+    }
+    
+    public Attributes attributes(){
+        return attributes; 
+    }
+    
+    public boolean hasAttribute(String name){ 
+        return attributes.contains(name);
+    }
+    
+    public String attributeValue(String attributeName){ 
+        return attributes.get(attributeName); 
+    }
+    
+    public String attributeValue(String attributeName, String defaultValue){ 
+        return attributes.get(attributeName, defaultValue);
+    }
+    
+    public int attributeValue(String attributeName, int defaultValue){ 
+        return attributes.get(attributeName, defaultValue);
+    }
+    
+}
