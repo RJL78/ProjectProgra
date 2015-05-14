60911ba163debdcd2e418f8f7b78cde76cc9fd54
romi514 <romain.leteurtre@gmail.com>
2015-05-14 19:16:22 +0200

    boloh

--- src/ch/epfl/imhof/dem/ReliefShader.java
+++ src/ch/epfl/imhof/dem/ReliefShader.java
@@ -6,9 +6,11 @@ import java.awt.image.Kernel;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import ch.epfl.imhof.PointGeo;
 import ch.epfl.imhof.Vector3;
+import ch.epfl.imhof.geometry.Point;
 import ch.epfl.imhof.painting.Color;
 import ch.epfl.imhof.projection.Projection;
 
@@ -24,12 +26,23 @@ public class ReliefShader {
        this.lightVector=lightVector;
     }
     
-    public Color[][] shadedRelief(PointGeo bottomLeftPoint, PointGeo topRightPoint, int pixelHeight, int pixelWidth, int blurRadius) {
+    public BufferedImage shadedRelief(Point bottomLeftPoint, Point topRightPoint, int pixelHeight, int pixelWidth, int blurRadius) {
         return null;
     }
    
-    private Color[][] shadedReliefRaw(int pixelHeight, int pixelWidth,Projection proj) {
-        return null;
+    private BufferedImage shadedReliefRaw(int pixelHeight, int pixelWidth, Function<Point,Point> coordinateChange) {
+        BufferedImage imageRelief = new BufferedImage(pixelWidth,pixelHeight,BufferedImage.TYPE_INT_RGB);
+        for (int i=0;i<pixelWidth;i++) {
+            for (int j=0;j<pixelHeight;j++) {
+                Point mapPoint = coordinateChange.apply(new Point(i,j));
+                PointGeo pointGeo = projection.inverse(mapPoint);
+                Vector3 pointVector = model.normalAt(pointGeo);
+                double angleCos = pointVector.scalarProduct(lightVector)/(pointVector.norm()*lightVector.norm());
+                Color newColor = Color.rgb(0.5*(angleCos+1),0.5*(angleCos+1),0.5*(0.7*angleCos+1));
+                imageRelief.setRGB(i, j, newColor.toAPIColor().getRGB());
+            }
+        }
+        return imageRelief;
     }
     
     private ConvolveOp gaussianVerticalKernel(double radius) {
@@ -38,7 +51,7 @@ public class ReliefShader {
         int n = (int) (2*r+1);
         float[] data = new float[n];
         float sum = 0;
-        List<Double> gausFilter = new ArrayList<>();
+        List<Double> gaussFilter = new ArrayList<>();
         for (int i= -r; i<=r; i++){
            float a = (float)Math.exp(-(i*i)/(2*o));
            data[i+r]= a;
@@ -47,12 +60,11 @@ public class ReliefShader {
         for (int i=0; i<= 2*r+1; i++){
              data[i] /= sum;
         }
-        return new ConvolveOp (new Kernel(1, n,data));
-           
+        return new ConvolveOp (new Kernel(1, n,data));   
     }
-
     
-    private Object wtf(Color[][] map, int radius) {
-        return null;
+    private BufferedImage applyKernel(BufferedImage image, ConvolveOp verticalConvolution) {
+        ConvolveOp horizontalConvolution = new ConvolveOp(new Kernel(verticalConvolution.getKernel().getHeight(),verticalConvolution.getKernel().getWidth(),verticalConvolution.getKernel().getKernelData(null)));
+        return horizontalConvolution.filter(verticalConvolution.filter(image,null), null);
     }
 }
