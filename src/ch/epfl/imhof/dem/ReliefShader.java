63626a94bc376fdbc48d41387b1c0dc4251dcf65
Raphael <raphael.laporte@live.com>
2015-05-14 18:48:05 +0200

    ok

--- src/ch/epfl/imhof/dem/ReliefShader.java
+++ src/ch/epfl/imhof/dem/ReliefShader.java
@@ -1,5 +1,12 @@
 package ch.epfl.imhof.dem;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
 import ch.epfl.imhof.PointGeo;
 import ch.epfl.imhof.Vector3;
 import ch.epfl.imhof.painting.Color;
@@ -25,9 +32,25 @@ public class ReliefShader {
         return null;
     }
     
-    private Object gaussianKernel(int radius) {
-        return null;
+    private ConvolveOp gaussianVerticalKernel(double radius) {
+        double o = radius/3;
+        int r = (int) Math.ceil(radius);
+        int n = (int) (2*r+1);
+        float[] data = new float[n];
+        float sum = 0;
+        List<Double> gausFilter = new ArrayList<>();
+        for (int i= -r; i<=r; i++){
+           float a = (float)Math.exp(-(i*i)/(2*o));
+           data[i+r]= a;
+           sum += a*a ;           
+        }
+        for (int i=0; i<= 2*r+1; i++){
+             data[i] /= sum;
+        }
+        return new ConvolveOp (new Kernel(1, n,data));
+           
     }
+
     
     private Object wtf(Color[][] map, int radius) {
         return null;
