9c9d66843efa12137e6f888941f6fb85f10ca8e9
romi514 <romain.leteurtre@gmail.com>
2015-05-16 20:39:47 +0200

    etapes7+8 commentaires

--- src/ch/epfl/imhof/painting/Canvas.java
+++ src/ch/epfl/imhof/painting/Canvas.java
@@ -3,9 +3,27 @@ package ch.epfl.imhof.painting;
 import ch.epfl.imhof.geometry.PolyLine;
 import ch.epfl.imhof.geometry.Polygon;
 
+/** 
+ * Interface représentant une toile
+ * 
+ * @author Raphael Laporte (251209) / Romain Leteurtre (238162)
+ */
 public interface Canvas {
     
+    /**
+     * Permet de dessiner sur la toile une polyligne donnée avec un style de ligne donné
+     * 
+     * @param polyline : la polyligne à dessiner
+     * @param lineStyle : le style de ligne à utiliser
+     */
     public void drawPolyline(PolyLine polyline, LineStyle lineStyle);
+    
+    /**
+     * Permet de dessiner sur la toile un polygone donnée avec une couleur donnée
+     * 
+     * @param polygon : le polygone à dessiner
+     * @param color : la couleur à utiliser
+     */
     public void drawPolygon(Polygon polygon, Color color);
 
 }
--- src/ch/epfl/imhof/painting/Painter.java
+++ src/ch/epfl/imhof/painting/Painter.java
@@ -11,71 +11,168 @@ import ch.epfl.imhof.geometry.PolyLine;
 import ch.epfl.imhof.painting.LineStyle.JOINT;
 import ch.epfl.imhof.painting.LineStyle.LINE_END;
 
+/** 
+ * Interface fonctionnelle représentant un peintre 
+ * 
+ * @author Raphael Laporte (251209) / Romain Leteurtre (238162)
+ */
 public interface Painter {
      
-    public void drawMap(Canvas c, Map map);
+    /**
+     * Permet de dessiner la carte sur la toile
+     * 
+     * @param canvas
+     * @param map
+     */
+    public void drawMap(Canvas canvas, Map map);
     
+    
+    /**
+     * Crée un peintre qui dessine l'interieur de tous les polygons de la carte avec une couleur donnée
+     * 
+     * @param color : couleur de dessin
+     * 
+     * @return un peintre dessinant l'interieur de tous les polygons de la carte avec la couleur donnée
+     */
     public static Painter polygon(Color color) {
-        return (c,m) -> {
-            for (Attributed<Polygon> attributedPolygon : m.polygons()) {
-                c.drawPolygon(attributedPolygon.value(),color);
+        return (canvas,map) -> {
+            for (Attributed<Polygon> attributedPolygon : map.polygons()) {
+                canvas.drawPolygon(attributedPolygon.value(),color);
             }
         };
     }
     
+    /**
+     * Crée un peintre dessinant toutes les lignes de la carte qu'on lui fournit avec un style donné
+     * 
+     * @param style : style des lignes
+     * 
+     * @return peintre dessinant toutes les lignes de la carte avec le style donné
+     */
     public static Painter line(LineStyle style) {
-        return (c,m) -> {
-            for (Attributed<PolyLine> attributedPolyline : m.polyLines()) {
-                c.drawPolyline(attributedPolyline.value(), style);
+        return (canvas,map) -> {
+            for (Attributed<PolyLine> attributedPolyline : map.polyLines()) {
+                canvas.drawPolyline(attributedPolyline.value(), style);
             }
         };
     }
     
+    /**
+     * Crée un peintre dessinant toutes les lignes de la carte qu'on lui fournit avec un style correspondant
+     * aux éléments de style donnés
+     * 
+     * @param color : couleur de la ligne
+     * @param thickness : épaisseur de la ligne
+     * @param lineEnd : terminaision de la ligne
+     * @param joint : jointure de la ligne
+     * @param lineSequence : séquence d'alternance des segments  
+     * 
+     * @return peintre dessinant toutes les lignes de la carte avec le style correspondant aux éléments donnés
+     */
     public static Painter line(Color color, float thickness, LINE_END lineEnd, JOINT joint, float[] lineSequence) {
         return line(new LineStyle(color, thickness, lineEnd, joint, lineSequence));
     }
     
+    /**
+     * Crée un peintre dessinant toutes les lignes de la carte qu'on lui fournit avec un style correspondant
+     * aux éléments de style donnés
+     * 
+     * @param color : couleur de la ligne
+     * @param thickness : épaisseur de la ligne
+     * 
+     * @return peintre dessinant toutes les lignes de la carte avec le style correspondant aux éléments donnés
+     */
     public static Painter line(Color color, float thickness) {
         return line(new LineStyle(color,thickness));
     }
     
+    /**
+     * Crée un peintre dessinant les pourtours de l'enveloppe et des trous de tous les polygones de la carte
+     * qu'on lui fournit avec un style donné
+     * 
+     * @param style : style des lignes
+     * 
+     * @return peintre dessinant les pourtours de l'enveloppe et des trous de tous les polygones de la carte avec le style donné
+     */
     public static Painter outline(LineStyle style) {
-        return (c,m) -> {
-        
-            for (Attributed<Polygon> attributedPolygon : m.polygons()) {
-                c.drawPolyline(attributedPolygon.value().shell(), style);
-                for (PolyLine polyline : attributedPolygon.value().holes()) {
-                    c.drawPolyline(polyline, style);
+        return (canvas,map) -> {
+            for (Attributed<Polygon> attributedPolygon : map.polygons()) {
+                canvas.drawPolyline(attributedPolygon.value().shell(), style);
+                for (PolyLine polylineHole : attributedPolygon.value().holes()) {
+                    canvas.drawPolyline(polylineHole, style);
                 }
             }
         };
     }
     
+    /**
+     * Crée un peintre dessinant les pourtours de l'enveloppe et des trous de tous les polygones de la carte qu'on lui fournit
+     * avec un style correspondant aux éléments de style donnés
+     * 
+     * @param color : couleur de la ligne
+     * @param thickness : épaisseur de la ligne
+     * @param lineEnd : terminaision de la ligne
+     * @param joint : jointure de la ligne
+     * @param lineSequence : séquence d'alternance des segments  
+     * 
+     * @return peintre dessinant les pourtours de l'enveloppe et des trous de tous les polygones de la carte
+     * avec le style correspondant aux éléments de style donnés
+     */
     public static Painter outline(Color color, float thickness, LINE_END lineEnd, JOINT joint, float[] lineSequence) {
         return outline(new LineStyle(color, thickness, lineEnd, joint, lineSequence));
     }
     
+    /**
+     * Crée un peintre dessinant les pourtours de l'enveloppe et des trous de tous les polygones de la carte qu'on lui fournit
+     * avec un style correspondant aux éléments de style donnés
+     * 
+     * @param color : couleur de la ligne
+     * @param thickness : épaisseur de la ligne  
+     * 
+     * @return peintre dessinant les pourtours de l'enveloppe et des trous de tous les polygones de la carte
+     * avec le style correspondant aux éléments de style donnés
+     */
     public static Painter outline(Color color, float thickness) {
         return outline(new LineStyle(color,thickness));
     }
     
+    /**
+     * Retourne un peintre se comportant comme celui auquel on l'applique,
+     * mais considèrent que les éléments de la carte satisfaisant le prédicat donné
+     * 
+     * @param pred : le prédicat qui filtre les élément de la carte
+     * 
+     * @return peintre comme celui auquel on l'applique qui ne considère que les éléments satisfaisant le prédicat donné
+     */
     public default Painter when(Predicate<Attributed<?>> pred) {
-        return (c,m) -> {
-            List<Attributed<PolyLine>> polylines = new ArrayList<>(m.polyLines());
-            List<Attributed<Polygon>> polygons = new ArrayList<>(m.polygons());
+        return (canvas,map) -> {
+            List<Attributed<PolyLine>> polylines = new ArrayList<>(map.polyLines());
+            List<Attributed<Polygon>> polygons = new ArrayList<>(map.polygons());
             polygons.removeIf(pred.negate());
             polylines.removeIf(pred.negate());
-            this.drawMap(c,new Map(polylines,polygons));
+            drawMap(canvas,new Map(polylines,polygons));
         };
     }
-    
+    /**
+     * Retourne un peintre dessinant d'abord la carte produite par le peintre pris en argument et,
+     * par dessus, la carte produite par le peintre auquel on l'applique
+     * 
+     * @param p : peintre utilisé en premier
+     * 
+     * @return un peintre qui est la composition des deux peintres
+     */
     public default Painter above(Painter p) {
-        return (c,m) -> {
-            p.drawMap(c,m);
-            this.drawMap(c,m);
+        return (canvas,map) -> {
+            p.drawMap(canvas,map);
+            this.drawMap(canvas,map);
         };
     }
     
+    /**
+     * Retourne un peintre utilisant l'attribut layer attaché aux entités de la carte pour la dessiner par couches
+     * 
+     * @return le peintre dessinant par couche
+     */
     public default Painter layered() {
         return layered(-5,5);
     }
